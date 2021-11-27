a96ba7e3ccf79a28272c684ab3d1b8c53d735120
Srivathssan V <srivathsan861@gmail.com>
2021-11-28 03:27:28 +0530

    create state for the component-cart item

--- react/cart/src/cartItem.js
+++ react/cart/src/cartItem.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			price: 999,
+			title: "Mobile Phone",
+			qty: 1,
+			img: ''
+		}
+	}
 	render(){
+		const{price,title,qty} = this.state; // object destructuring
 		return(
 		<div className="class-item">
 
@@ -12,11 +22,15 @@ class CartItem extends React.Component{
 		 </div>
 
 		 <div className="right-block">
-		    <div style={{fontSize:25}}>Phone</div>
-		    <div style={{color:"grey"}}>Rs 999</div>
-		 	<div style={{color:"grey"}}>QTY:1</div>
+		    <div style={{fontSize:25}}>{title}</div>  {/*directly writing the object inside*/}
+		    <div style={{color:"grey"}}>{price}</div>
+		 	<div style={{color:"grey"}}>{qty}</div>
 		 	<div className="cart-item-actions">
 		 		{/* {Buttons} */}
+                <img alt="Increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1828/1828926.png"></img>
+                <img alt="Decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"></img>
+                <img alt="Delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"></img>
+
 		 	</div>
 		 </div>
 			
